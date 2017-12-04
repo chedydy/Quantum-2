80475c47ef534154d6ba430016400ff55050a5f6
Boc <radu.boc@haufe-lexware.com>
2017-12-04 16:48:49 +0200

    removed comments and unused code

--- src/Routers/AppRouter.js
+++ src/Routers/AppRouter.js
@@ -5,14 +5,6 @@ import { BrowserRouter, Route, Switch } from "react-router-dom";
 import PublicLayout from "./PublicLayout";
 import AdminLayout from "./AdminLayout";
 
-import Home from "../Components/Home";
-import About from "../Components/About";
-import Post from "../Components/Post";
-import Contact from "../Components/Contact";
-import NotFoundPage from "../Components/NotFound";
-import Main from "../Components/Main";
-import PostsPreview from "../Components/PostsPreview";
-
 const AppRouter = () => (
   <BrowserRouter>
     <div>
@@ -20,14 +12,6 @@ const AppRouter = () => (
         <Route path="/admin" component={AdminLayout} />
         <Route path="/" component={PublicLayout} />
       </Switch>
-      {/* <Switch>
-        <Route path="/" component={Home} exact={true} />
-        <Route path="/about" component={About} />
-        <Route path="/posts/:id" component={Post} />
-        <Route path="/posts" component={PostsPreview} />
-        <Route path="/contact" component={Contact} />
-        <Route component={NotFoundPage} />
-      </Switch> */}
     </div>
   </BrowserRouter>
 );
