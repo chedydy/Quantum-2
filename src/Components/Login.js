b33f458428b502e78091637fe89fdf7bd01a1c2f
IonescuA <alina.ionescu@haufe-lexware.com>
2017-12-06 02:59:06 +0200

    added FacebookLogin button

--- src/Components/Login.js
+++ src/Components/Login.js
@@ -1,5 +1,7 @@
 import React, { Component } from "react";
 import { provider, auth } from "../firebase/firebase";
+import {FacebookLoginButton} from 'react-social-login-buttons';
+import LoginImg from "../img/QfbLogin.png";
 
 class Login extends Component {
   facebookLogin = () => {
@@ -14,10 +16,44 @@ class Login extends Component {
   render() {
     return (
       <div>
-        <button onClick={this.facebookLogin.bind(this)}>Login</button>
+        <div style={container}>
+            <div style={centerButton}>
+              <FacebookLoginButton text="Login with Facebook" onClick={this.facebookLogin.bind(this)} />
+            </div>
+        </div>  
       </div>
     );
   }
 }
+
+const centerButton = {
+  width: 'auto',
+  textAlign: 'left',
+  marginBottom: 'auto',
+  marginTop: '20px',
+  marginRight: 'auto',
+  marginLeft: 'auto',
+};
+
+const container = {
+  background: 'url("'+LoginImg+'") no-repeat center fixed',
+  backgroundColor: 'black',
+  backgroundSize: 'contain',
+  /* Set rules to fill background */
+  minHeight: '100%',
+  minWidth: '1024px',
+  /* Set up proportionate scaling */
+  width: '100%',
+  height: 'auto',
+	
+  /* Set up positioning */
+  position: 'fixed',
+  top: 0,
+  left: 0,
+
+  display: 'flex',
+  flexDirection: 'column',
+  height: '500px'
+};
 
 export { Login };
