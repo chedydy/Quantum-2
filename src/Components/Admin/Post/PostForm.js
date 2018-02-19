e5acb6da73300f7e4aa3fbd99937f3facdb84206
Boc <radu.boc@haufe-lexware.com>
2018-02-19 19:55:47 +0200

    added md helpers

--- src/Components/Admin/Post/PostForm.js
+++ src/Components/Admin/Post/PostForm.js
@@ -1,13 +1,89 @@
 import React, { Component } from "react";
-import {
-  Input,
-  Textarea,
-  FileInput,
-  SelectInput
-} from "../../Common";
+import { Input, Textarea, FileInput, SelectInput } from "../../Common";
 import "./EditPost.css";
 
 class PostForm extends Component {
+  state = { start: 0, end: 0, url: "", imageUrl: "" };
+  updateState(field, value) {
+    this.setState({ [field]: value });
+  }
+  italic() {
+    const text = this.props.post.content.slice(
+      this.state.start,
+      this.state.end
+    );
+    const textWithQuote = `*${text}*`;
+    var newContent = [
+      this.props.post.content.slice(0, this.state.start),
+      textWithQuote,
+      this.props.post.content.slice(this.state.end)
+    ].join("");
+    this.props.updateProps("post", "content", newContent);
+  }
+  bold() {
+    const text = this.props.post.content.slice(
+      this.state.start,
+      this.state.end
+    );
+    const textWithQuote = `**${text}**`;
+    var newContent = [
+      this.props.post.content.slice(0, this.state.start),
+      textWithQuote,
+      this.props.post.content.slice(this.state.end)
+    ].join("");
+    this.props.updateProps("post", "content", newContent);
+  }
+  addHeader() {
+    var newContent = [
+      this.props.post.content.slice(0, this.state.start),
+      "###",
+      this.props.post.content.slice(this.state.start)
+    ].join("");
+    this.props.updateProps("post", "content", newContent);
+  }
+  addImageUrl() {
+    const text = this.props.post.content.slice(
+      this.state.start,
+      this.state.end
+    );
+    const textWithUrl = `![${text}](${this.state.imageUrl} "${text}")`;
+    var newContent = [
+      this.props.post.content.slice(0, this.state.start),
+      textWithUrl,
+      this.props.post.content.slice(this.state.end)
+    ].join("");
+    this.props.updateProps("post", "content", newContent);
+  }
+  addUrl() {
+    const text = this.props.post.content.slice(
+      this.state.start,
+      this.state.end
+    );
+    const textWithUrl = `[${text}](${this.state.url})`;
+    var newContent = [
+      this.props.post.content.slice(0, this.state.start),
+      textWithUrl,
+      this.props.post.content.slice(this.state.end)
+    ].join("");
+    this.props.updateProps("post", "content", newContent);
+  }
+  addQuote() {
+    const text = this.props.post.content.slice(
+      this.state.start,
+      this.state.end
+    );
+    const textWithQuote = `> ${text}`;
+    var newContent = [
+      this.props.post.content.slice(0, this.state.start),
+      textWithQuote,
+      this.props.post.content.slice(this.state.end)
+    ].join("");
+    this.props.updateProps("post", "content", newContent);
+  }
+  onSelectContent(start, end) {
+    this.setState({ start, end });
+  }
+
   render() {
     return (
       <div>
@@ -60,6 +136,40 @@ class PostForm extends Component {
             this.props.updateProps("post", "imageUrl", imageUrl);
           }}
         />
+        <div>
+          <button onClick={this.italic.bind(this)} type="button">
+            Italic
+          </button>
+          <button onClick={this.bold.bind(this)} type="button">
+            Bold
+          </button>
+          <button onClick={this.addHeader.bind(this)} type="button">
+            Add header
+          </button>
+          <input
+            placeholder="Enter url"
+            label="Url"
+            type="text"
+            value={this.state.url}
+            onChange={e => this.updateState("url", e.target.value)}
+          />
+          <button onClick={this.addUrl.bind(this)} type="button">
+            Add link
+          </button>
+          <input
+            placeholder="Enter image url"
+            label="Image url"
+            type="text"
+            value={this.state.imageUrl}
+            onChange={e => this.updateState("imageUrl", e.target.value)}
+          />
+          <button onClick={this.addImageUrl.bind(this)} type="button">
+            Add image
+          </button>
+          <button onClick={this.addQuote.bind(this)} type="button">
+            Add quote
+          </button>
+        </div>
         <Textarea
           id="content"
           placeholder="Content"
@@ -67,6 +177,7 @@ class PostForm extends Component {
           rows="10"
           value={this.props.post.content}
           onChange={value => this.props.updateProps("post", "content", value)}
+          onSelect={this.onSelectContent.bind(this)}
         />
         <br />
       </div>
--- src/Components/Common/Textarea.js
+++ src/Components/Common/Textarea.js
@@ -1,21 +1,20 @@
 import React from "react";
 
 const Textarea = props => {
-  
   return (
-    
     <div className="control-group">
       <div className="form-group floating-label-form-group controls">
         <label>{props.label}</label>
         <textarea
           rows={props.rows || "5"}
-          style={{zIndex:'unset'}}
+          style={{ zIndex: "unset" }}
           className="form-control"
           placeholder={props.placeholder}
           id={props.id}
           required={props.required}
           value={props.value || ""}
-          onChange={(e)=>props.onChange(e.target.value)}
+          onChange={e => props.onChange(e.target.value)}
+          onSelect={e => props.onSelect(e.target.selectionStart,e.target.selectionEnd)}
         />
       </div>
     </div>
