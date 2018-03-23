fed2564b6b58f562a5e4f1c64c1d8cbe7980a248
Diana Panta <diana-maria.panta@haufe-lexware.com>
2018-03-23 16:04:35 +0200

    about content

--- src/Components/Public/AboutContent.js
+++ src/Components/Public/AboutContent.js
@@ -1,63 +1,167 @@
-import React, {Component} from "react";
+import React, { Component } from "react";
 import ReactMarkdown from "react-markdown";
 import { PageHeader } from "./PageHeader";
 import { Container } from "../Common";
-import contact1 from "../../img/blue_space_nebula.jpg";
+// import CoverImg from "../../img/coverpic.png";
+// import contact1 from "../../img/blue_space_nebula.jpg";
 import "./AboutContent.css";
-import "./AboutContent.scss";
+// import "./AboutContent.scss";
+// import "./Grid.scss"
 
 class AboutContent extends Component {
   // state = {   visible: false };
-  state ={
+  state = {
     display: false
-  }
+  };
   onShow() {
-    this.setState({display: !this.state.display});
+    this.setState({ display: !this.state.display });
   }
   render() {
-    const {about} = this.props;
+    const { about } = this.props;
     return (
       <div className="myheader">
         <PageHeader image={about.imageUrl} title={"About Me"}>
           {about.title}
         </PageHeader>
+
         <Container>
           <div
             style={{
               textAlign: "justify"
             }}
           >
-            <ReactMarkdown source={about.content} />
+            <ReactMarkdown source={about.content} />{" "}
           </div>
-          <br />
-          <hr />
-          <div className="text-aspect">
-            <div>
-              <div className="the-div">
-                <div className="the-divs">
-                  <h3 className="name-style">Victor Jivanescu</h3>
-                  <div className="the-details-div" onClick={this.onShow.bind(this)}>
-                    <i className="fas fa-caret-right" />
-                    <span className="padding-arrow ">Show More</span>
-                  </div>
-                  <div style={{display: this.state.display? "block" : "none"}} className="rectangle" />
-                </div>
-                <div className="the-divs">
-                  <img src={contact1} className="img-style" alt="logo" />
-                </div>
+          <div className="wrap-contacts">
+            <div className="container-avatar">
+              <img src={about.imageUrl} alt="Avatar" className="image" />
+              <div className="overlay">
+                <div className="text">Hello World</div>
               </div>
-              About Simon Stewart Simon Stewart is the creator of WebDriver, the
-              open source web application testing tool, as well as a core
-              Selenium developer. WebDriver remains a hot topic as it is
-              currently going through a W3C (World Wide Web Consortium)
-              specification process, which Simon is a co-editor of. He describes
-              himself as “undeniably hairy”, and holds a BSc in computer science
-              from Nottingham University.
             </div>
             <br />
+            <div className="container-avatar">
+              <img src={about.imageUrl} alt="Avatar" className="image" />
+              <div className="overlay">
+                <div className="text">Hello World</div>
+              </div>
+            </div>
+            <br />
+            <div className="container-avatar">
+              <img src={about.imageUrl} alt="Avatar" className="image" />
+              <div className="overlay">
+                <div className="text">Hello World</div>
+              </div>
+            </div>
+            <br />
+            <div className="container-avatar">
+              <img src={about.imageUrl} alt="Avatar" className="image" />
+              <div className="overlay">
+                <div className="text">Hello World</div>
+              </div>
+            </div>
           </div>
         </Container>
       </div>
+      /* <div className="row">
+                    <div className="col-1-of-3">
+                       <div className="card">
+                           <div className="card__side card__side--front">
+                                <div className="card__picture card__picture--1">
+                                    &nbsp;
+                                </div>
+                                <h4 className="card__heading">
+                                    <span className="card__heading-span card__heading-span--1">The Sea Explorer</span>
+                                </h4>
+                                <div className="card__details">
+                                    <ul>
+                                        <li>3 day tours</li>
+                                        <li>Up to 30 people</li>
+                                        <li>2 tour guides</li>
+                                        <li>Sleep in cozy hotels</li>
+                                        <li>Difficulty: easy</li>
+                                    </ul>
+                                </div>
+                           </div>
+                           <div className="card__side card__side--back card__side--back-1">
+                                <div className="card__cta">
+                                    <div className="card__price-box">
+                                        <p className="card__price-only">Only</p>
+                                        <p className="card__price-value">$297</p>
+                                    </div>
+                                    <a href="#popup" className="btn btn--white">Book now!</a>
+                                </div>
+                            </div>
+                       </div>
+                    </div>
+
+
+                    <div className="col-1-of-3">
+                        <div className="card">
+                            <div className="card__side card__side--front">
+                                <div className="card__picture card__picture--2">
+                                    &nbsp;
+                                </div>
+                                <h4 className="card__heading">
+                                    <span className="card__heading-span card__heading-span--2">The Forest Hiker</span>
+                                </h4>
+                                <div className="card__details">
+                                    <ul>
+                                        <li>7 day tours</li>
+                                        <li>Up to 40 people</li>
+                                        <li>6 tour guides</li>
+                                        <li>Sleep in provided tents</li>
+                                        <li>Difficulty: medium</li>
+                                    </ul>
+                                </div>
+
+                            </div>
+                            <div className="card__side card__side--back card__side--back-2">
+                                <div className="card__cta">
+                                    <div className="card__price-box">
+                                        <p className="card__price-only">Only</p>
+                                        <p className="card__price-value">$497</p>
+                                    </div>
+                                    <a href="#popup" className="btn btn--white">Book now!</a>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+
+
+                    <div className="col-1-of-3">
+                        <div className="card">
+                            <div className="card__side card__side--front">
+                                <div className="card__picture card__picture--3">
+                                    &nbsp;
+                                </div>
+                                <h4 className="card__heading">
+                                    <span className="card__heading-span card__heading-span--3">The Snow Adventurer</span>
+                                </h4>
+                                <div className="card__details">
+                                    <ul>
+                                        <li>5 day tours</li>
+                                        <li>Up to 15 people</li>
+                                        <li>3 tour guides</li>
+                                        <li>Sleep in provided tents</li>
+                                        <li>Difficulty: hard</li>
+                                    </ul>
+                                </div>
+
+                            </div>
+                            <div className="card__side card__side--back card__side--back-3">
+                                <div className="card__cta">
+                                    <div className="card__price-box">
+                                        <p className="card__price-only">Only</p>
+                                        <p className="card__price-value">$897</p>
+                                    </div>
+                                    <a href="#popup" className="btn btn--white">Book now!</a>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+      </div> }*/
     );
   }
 }
