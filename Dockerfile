2b8ee57935082066eb7d74651e37f601f558b20f
Boc <radu.boc@haufe-lexware.com>
2018-05-30 10:49:48 +0300

    fixed deploy port

--- Dockerfile
+++ Dockerfile
@@ -3,5 +3,4 @@ RUN mkdir -p /usr/src/app
 WORKDIR /usr/src/app
 COPY build /usr/src/app/
 RUN npm install -g serve
-RUN ls
 CMD [ "serve", "-s", "." ]
--- deployscripts/deploy.ps1
+++ deployscripts/deploy.ps1
@@ -1,2 +1,5 @@
 kubectl delete -f quantum-deployment.yaml
+kubectl delete -f quantum-service.yaml
+
 kubectl apply -f quantum-deployment.yaml
+kubectl apply -f quantum-service.yaml
